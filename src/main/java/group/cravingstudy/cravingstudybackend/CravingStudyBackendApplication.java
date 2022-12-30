856ea9ec860ae0252f3f148e9bd7b91db79ff086
D <oncwnuIpTUDmQpWn3HCZzHgp2EEQ@git.weixin.qq.com>
2022-12-30 13:59:19 +0800

    First Commit

---
+++ src/main/java/group/cravingstudy/cravingstudybackend/CravingStudyBackendApplication.java
@@ -0,0 +1,14 @@
+package group.cravingstudy.cravingstudybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class CravingStudyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CravingStudyBackendApplication.class, args);
+    }
+}
